f0eca5772564ffb6d51dc231f7859e2ed7358f5d
lumberjack-programmer <dmitry.p12@gmail.com>
2022-09-08 17:36:33 +0200

    Removed some code commented out

--- lib/components/custom_search_delegate.dart
+++ lib/components/custom_search_delegate.dart
@@ -63,65 +63,7 @@ class CustomSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    if (query.length < 3) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: Text(
-              "Search term must be longer than two letters.",
-            ),
-          )
-        ],
-      );
-    }
-
     return Container();
-
-    // InheritedBlocs.of(context)
-    //     .searchBloc
-    //     .searchTerm
-    //     .add(query);
-
-    // return Column(
-    //   children: <Widget>[
-    //     //Build the results based on the searchResults stream in the searchBloc
-    //     StreamBuilder(
-    //       stream: InheritedBlocs.of(context).searchBloc.searchResults,
-    //       builder: (context, AsyncSnapshot<List<Result>> snapshot) {
-    //         if (!snapshot.hasData) {
-    //           return Column(
-    //             crossAxisAlignment: CrossAxisAlignment.center,
-    //             mainAxisAlignment: MainAxisAlignment.center,
-    //             children: <Widget>[
-    //               Center(child: CircularProgressIndicator()),
-    //             ],
-    //           );
-    //         } else if (snapshot.data.length == 0) {
-    //           return Column(
-    //             children: <Widget>[
-    //               Text(
-    //                 "No Results Found.",
-    //               ),
-    //             ],
-    //           );
-    //         } else {
-    //           var results = snapshot.data;
-    //           return ListView.builder(
-    //             itemCount: results.length,
-    //             itemBuilder: (context, index) {
-    //               var result = results[index];
-    //               return ListTile(
-    //                 title: Text(result.title),
-    //               );
-    //             },
-    //           );
-    //         }
-    //       },
-    //     ),
-    //   ],
-    // );
-
   }
 
 
